dc9d358db69bb3d3ed099861b9472b7c740aab41
hmcallen@outlook.com <hmcallen@outlook.com>
2022-01-25 14:03:37 +0000

    wek 2 lab

---
+++ lab2.py
@@ -0,0 +1,14 @@
+friends = ['alif', 'kyle','ryan', 'andrew',"keevin"]
+print(friends)
+print(len(friends))
+friends.append("Harry")
+print(friends)
+friends.insert(1,"Ben")
+print(friends)
+del friends[1]
+print(friends)
+friends.remove('Harry')
+print(friends)
+popped_friends = friends.pop()
+print(friends)
+print(popped_friends)
